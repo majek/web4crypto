d093efb8fd1d7bbd141e136b923219e31abb2347
Marek Majkowski <majek04@gmail.com>
2013-03-12 03:55:37 +0000

    Use template for exdercise3

--- timed_pw.py
+++ timed_pw.py
@@ -1,41 +1,40 @@
-#!/usr/bin/env python3
-
 import string
 import time
 import random
 import bottle
 import os
 
+import yaml
 
-# random uses int(time.time() * 256) when it's first time loaded
-# make sure we call random.choice soon after
+with open('config.yaml') as f:
+    config = yaml.load(f.read())['exercise3']
+
+
 SECRET_LEN = 12
 
-secret_time = time.time()
+secret_time = config.get('secret_time', None) or time.time()
 
-# this is the default for systems without _urandom, btw.
+# This is the default seed for systems without _urandom
 random.seed(int(secret_time * 256))
 
 secret = ''.join(random.choice(string.ascii_letters) for i in range(SECRET_LEN))
-print("[.] Secret_time: %r %r" % (secret_time, int(secret_time * 256)))
+print("[.] Secret_time: %r" % (secret_time,))
 print("[.] Secret: %r" % (secret,))
-
-
 
 
 app = bottle.Bottle()
 
-@app.get('/')
-def login_form(message = ''):
-    return message + \
-            '<p>Password of length {0} was generated {1} seconds ago.</p>'.\
-            format(SECRET_LEN, int(time.time() - secret_time))
+@app.get('/', template='exercise3.html')
+def login_form():
+    return {'secret_len': SECRET_LEN,
+            'secret_delta_time': int(time.time() - secret_time)}
 
 @app.post('/')
 def login_submit():
     password = bottle.request.forms.get('password')
     if password == secret:
-        return '''<p>Your password was CORRECT! How's that possible! OMG!</p>'''
+        return "Your password was CORRECT! How's that possible! OMG! " \
+               "Your reward, the secret token (save it!): %r" % (config['token'],)
     else:
         bottle.abort(401, '''Your password was incorrect; try again.''')
 
